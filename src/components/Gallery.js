21598c3ea6d60f9a94a8f16b4aaab73c3ee1334d
Mohamed Tarakan <moetarakan@mohameds-air.lan>
2024-02-26 17:56:49 -0500

    created withContext

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,10 @@
+import { useContext } from 'react'
+import { DataContext } from '../context/DataContext'
 import GalleryItem from './GalleryItem'
 
-function Gallery(props){
-    
-    const display = props.data.map((item, index) => {
+function Gallery(){
+    const data = useContext(DataContext)
+    const display = data.map((item, index) => {
         return (
             <GalleryItem item={item} key={index} />
         )
@@ -10,7 +12,7 @@ function Gallery(props){
   
     return (
         <div>
-        {display}
+            {display}
         </div>
     )
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,41 +1,15 @@
-import { useState } from 'react'
+import { useContext } from 'react'
+import { SearchContext } from '../context/SearchContext'
 
-function SearchBar(props){
-    let [searchTerm, setSearchTerm] = useState('')
+function SearchBar(){
+    let {term, handleSearch} = useContext(SearchContext)
 
     return (
-        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-
-            <input type="text" placeholder="Enter a search term here"
-            onChange={(e) => setSearchTerm(e.target.value)} />
-
-            <input type="submit" />
-
-       </form>
+        <form>
+            <input ref={term} type="text" placeholder="Search Here" />
+            <button type ="submit" onClick={(e) => handleSearch(e, term.current.value)}>Submit</button>
+        </form>
     )
 }
 
 export default SearchBar
-
-
-
-
-//import { useState } from 'react'
-
-//function SearchBar(props){
-    // We can comment out our searchTerm state variable as we are not using it!
-    // let [searchTerm, setSearchTerm] = useState('')
-
-//    return (
-//            <form>
-//                <input type="text" placeholder="Search Here"
-//                    onChange={
-//                        (e) => props.handleSearch(e, e.target.value)
-//                    } />
-//                <input type="submit" />
-//            </form>
-//    )
-//}
-
-//export default SearchBar
-  
